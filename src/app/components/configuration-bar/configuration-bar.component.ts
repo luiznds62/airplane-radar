79251869f4c771c2b6fe13ea5ebf14e057b19316
luiznds62 <luiznds62@gmail.com>
2021-07-01 21:03:07 -0500

    Ajusta responsividade

--- src/app/components/configuration-bar/configuration-bar.component.ts
+++ src/app/components/configuration-bar/configuration-bar.component.ts
@@ -52,37 +52,4 @@ export class ConfigurationBarComponent implements OnInit {
     this.showReportNotification = true;
   }
 
-  getRandomArbitrary(min: number, max: number) {
-    return Math.ceil(Math.random() * (max - min) + min);
-  }
-
-  deleteAll() {
-    this.reportMessage = '';
-    this.showReportNotification = false;
-    this.setCollection.emit([]);
-  }
-
-  generateExamples() {
-    const airplanes: Airplane[] = [];
-
-    [...Array(10).keys()].forEach(id => {
-      const x = this.getRandomArbitrary(-15, 15);
-      const y = this.getRandomArbitrary(-15, 15);
-
-      const airplane: Airplane = {
-        id: id + 1,
-        x,
-        y,
-        radius: this.applicationLogicService.calculatePolarCoordinatesRadius(x, y),
-        angle: this.applicationLogicService.calculatePolarCoordinatesAngle(x, y),
-        direction: this.getRandomArbitrary(0, 360),
-        speed: this.getRandomArbitrary(0, 100),
-        translation: 0,
-        selected: false,
-      };
-      airplanes.push(airplane);
-    });
-
-    this.setCollection.emit(airplanes);
-  }
 }
--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,15 +1,51 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { Airplane } from '../../models/Airplane';
+import { ApplicationLogicService } from '../../services/application-logic.service';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.css']
+  styleUrls: ['./navbar.component.css'],
 })
 export class NavbarComponent implements OnInit {
+  @Output() setCollection = new EventEmitter();
 
-  constructor() { }
+  constructor(private applicationLogicService: ApplicationLogicService) {
+  }
 
   ngOnInit(): void {
   }
 
+
+  getRandomArbitrary(min: number, max: number) {
+    return Math.ceil(Math.random() * (max - min) + min);
+  }
+
+  deleteAll() {
+    this.setCollection.emit([]);
+  }
+
+  generateExamples() {
+    const airplanes: Airplane[] = [];
+
+    [...Array(10).keys()].forEach(id => {
+      const x = this.getRandomArbitrary(-15, 15);
+      const y = this.getRandomArbitrary(-15, 15);
+
+      const airplane: Airplane = {
+        id: id + 1,
+        x,
+        y,
+        radius: this.applicationLogicService.calculatePolarCoordinatesRadius(x, y),
+        angle: this.applicationLogicService.calculatePolarCoordinatesAngle(x, y),
+        direction: this.getRandomArbitrary(0, 360),
+        speed: this.getRandomArbitrary(0, 100),
+        translation: 0,
+        selected: false,
+      };
+      airplanes.push(airplane);
+    });
+
+    this.setCollection.emit(airplanes);
+  }
 }
